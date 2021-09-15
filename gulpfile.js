94b00a916d853508e9ffcc402cbe652b62899fcc
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2021-09-15 12:20:33 +0100

    Commit site files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+
+function buildStyles() {
+  return src('./styles/sass/index.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['**/*.html'] }))
+    .pipe(dest('./styles/css'));
+}
+
+function watchTask() {
+  watch(['./styles/**/*.scss', '**/*.html'], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
